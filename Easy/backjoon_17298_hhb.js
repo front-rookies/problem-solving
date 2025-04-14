e273fe1e40895b35b45bbbbe62e94c6662319fec
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2025-04-14 22:53:56 +0900

    Create backjoon_17298_hhb.js

---
+++ Easy/backjoon_17298_hhb.js
@@ -0,0 +1,22 @@
+const input = require("fs")
+  .readFileSync("./dev/stdin.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .slice(1)[0]
+  .split(" ")
+  .map((e) => Number(e));
+const ans = () => {
+  const stack = [];
+  const ans = [];
+  for (let i = input.length - 1; i >= 0; i--) {
+    while (stack.length) {
+      if (stack[stack.length - 1] > input[i]) break;
+      stack.pop();
+    }
+    ans.push(stack.length ? stack[stack.length - 1] : -1);
+    stack.push(input[i]);
+  }
+  console.log(ans.reverse().join(" "));
+};
+ans();
